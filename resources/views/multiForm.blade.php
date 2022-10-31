83f3c474bfb296982a6d70d89dba5eca85a38c59
Frizi <frizi20@yahoo.com>
2022-10-31 08:48:11 +0200

    multi step form animation

--- resources/views/multiForm.blade.php
+++ resources/views/multiForm.blade.php
@@ -47,33 +47,74 @@
 			box-shadow: rgb(0 0 0 / 10%) 0px 1px 30px;
 		}
 
-		.progress {
-			width: 100%;
+		.progress-bar-container {
+			/* width: 100%;
 			height: 10px;
 			background: #9ad2f4;
 			margin-top: 10px;
-
+			position: relative;
 
 			color: white;
 			text-align: center;
 			line-height: 75px;
 			font-size: 35px;
 			font-family: "Segoe UI";
-			animation-direction: reverse;
-			background: #e5405e;
+			animation-direction: reverse; */
+
+			height: 10px;
+ 			 /* margin: 50px 0px; */
+ 			 background: rgb(255, 255, 255);
+  			position:relative; /* relative here */
+
+			/* background: #e5405e; */
 			/* Old browsers */
-			background: -moz-linear-gradient(left, #e5405e 0%, #ffdb3a 25%, #3fffa2 50%, #3fffa2 50%, #1a9be0 73%, #ba68ed 100%);
+			/* background: -moz-linear-gradient(left, #e5405e 0%, #ffdb3a 25%, #3fffa2 50%, #3fffa2 50%, #1a9be0 73%, #ba68ed 100%); */
 			/* FF3.6-15 */
-			background: -webkit-linear-gradient(left, #e5405e 0%, #ffdb3a 25%, #3fffa2 50%, #3fffa2 50%, #1a9be0 73%, #ba68ed 100%);
+			/* background: -webkit-linear-gradient(left, #e5405e 0%, #ffdb3a 25%, #3fffa2 50%, #3fffa2 50%, #1a9be0 73%, #ba68ed 100%); */
 			/* Chrome10-25,Safari5.1-6 */
-			background: linear-gradient(to right, #e5405e 0%, #ffdb3a 25%, #3fffa2 50%, #3fffa2 50%, #1a9be0 73%, #ba68ed 100%);
+			/* background: linear-gradient(to right, #e5405e 0%, #ffdb3a 25%, #3fffa2 50%, #3fffa2 50%, #1a9be0 73%, #ba68ed 100%); */
+		}
+
+		.progress-bar-indicator{
+			/* position: absolute;	 */
+			height: 100%;
+			/* width: 100%; */
+			/* overflow: hidden; */
+			width: 10px;
+			-webkit-mask:linear-gradient(#fff 0 0);
+          	mask:linear-gradient(#fff 0 0);
+			border-radius: 5px;
+			transition:width 0.2s linear ;
+		}
+
+		.progress-bar-indicator::before {
+			content:"";
+  			position:absolute;
+  			top:0;
+  			left:0;
+  			right:0;
+  			bottom:0;
+  			background-image: linear-gradient(to right, #166260, #07adce, #3b91ff);
+			border-radius: 5px;
+		}
+		
+
+		.multi-form-container .slider{
+			position: relative;
+			min-height: 150px;
+			overflow: hidden;
+
 		}
 
 		.multi-form-container fieldset {
+			width: 100%;
 			flex: 1;
 			border: none;
 			padding-bottom: 30px;
-
+			position: absolute;
+			top: 0;
+			left: 0;
+			transition: all 0~.5s ease-out;
 		}
 
 		.fs-title {
@@ -141,32 +182,47 @@
 	<div class="wrapper">
 
 		<div class="multi-form-container">
-			<div class="progress">
-
+			<div class="progress-bar-container">
+				<div class="progress-bar-indicator"></div>
 			</div>
+			<div class="slider">
+				<fieldset>
 
-			<fieldset>
-
-				<div class="fs-title">Intrebarea 4</div>
-				<div class="fs-subtitle">
-					What postgraduate qualifications do you hold?
-				</div>
-
-				<div class="input-slider">
-					<div class="fs-input">
-						<select name="" id="">
-							<option value="1">Bucuresti</option>
-							<option value="2">Brasov</option>
-							<option value="3">Cluj</option>
-						</select>
+					<div class="fs-title">Intrebarea 4</div>
+					<div class="fs-subtitle">
+						What postgraduate qualifications do you hold?
 					</div>
+	
+					<div class="input-slider">
+						<div class="fs-input">
+							<select name="" id="">
+								<option value="1">Bucuresti</option>
+								<option value="2">Brasov</option>
+								<option value="3">Cluj</option>
+							</select>
+						</div>
+	
+					</div>
+				</fieldset>
+				<fieldset>
 
-				</div>
-
-
-
-			</fieldset>
-
+					<div class="fs-title">Intrebarea 4</div>
+					<div class="fs-subtitle">
+						What postgraduate qualifications do you hold?
+					</div>
+	
+					<div class="input-slider">
+						<div class="fs-input">
+							<select name="" id="">
+								<option value="1">Bucuresti</option>
+								<option value="2">Brasov</option>
+								<option value="3">Cluj</option>
+							</select>
+						</div>
+	
+					</div>
+				</fieldset>
+			</div>
 			<div class="form-buttons">
 				<div class="prev-btn">Previous</div>
 				<div class="next-btn">Next</div>
@@ -179,14 +235,92 @@
 
 
 	<script>
+
+		// (function(){
+			document.querySelector('.slider').innerHTML =  document.querySelectorAll('.slider fieldset')[0].outerHTML.repeat(10)
+		// })();
+
 		const prevBtn = document.querySelector('.prev-btn');
 		const nextBtn = document.querySelector('.next-btn');
+		const questions =  document.querySelectorAll('.slider fieldset')
+		const slider = document.querySelector('.slider')
+		const progressBar = document.querySelector('.progress-bar-indicator')
 
+		let currQuestion = 0
+		let questionNr = questions.length;
+
+
+		console.log(questions)
 
 		nextBtn.addEventListener('click',(e)=>{
 			console.log(e)
 		})
 
+		document.querySelector('.form-buttons').addEventListener('click',(e)=>{
+			if(e.target.className.includes('next-btn')) nextQuestion()
+			if(e.target.className.includes('prev-btn')) prevQuestion()
+		})
+
+		const goToQuestion = function(questionNr){
+			Array.from(questions).forEach((question, i) => {
+				// console.log(`translateX(${100 * (i - questionNr)})`)
+				
+				// console.log(progressBar.style.width = '30%')
+				
+				const margin = 0
+				const scale = questionNr == i ? 'scale(1)' : 'scale(0.5)'
+				
+				question.style.transform = `translateX(${100 * (i - questionNr) + margin }%) ${scale}`
+
+				// progressBar.style.width = `${currQuestion / questionNr * 100}%`
+			});
+		}
+
+		function nextQuestion() {
+			if(currQuestion >= questionNr -1) return
+			currQuestion++
+			goToQuestion(currQuestion)
+			updateProgressBar()
+		}
+
+		function prevQuestion(){
+			if(currQuestion === 0) return
+			currQuestion--
+			goToQuestion(currQuestion)
+			updateProgressBar()
+		}
+
+		function updateProgressBar(){
+			progressBar.style.width =( currQuestion + 1) / questionNr * 100 + '%'
+		}
+
+		goToQuestion(currQuestion)
+		updateProgressBar(1)
+
+
+
+
+		function createField(){
+				// <fieldset>
+
+				// 	<div class="fs-title">Intrebarea 4</div>
+				// 	<div class="fs-subtitle">
+				// 		What postgraduate qualifications do you hold?
+				// 	</div>
+	
+				// 	<div class="input-slider">
+				// 		<div class="fs-input">
+				// 			<select name="" id="">
+				// 				<option value="1">Bucuresti</option>
+				// 				<option value="2">Brasov</option>
+				// 				<option value="3">Cluj</option>
+				// 			</select>
+				// 		</div>
+	
+				// 	</div>
+				// </fieldset>
+		}
+		
 
 	</script>
 

a0fb406173e1a34477790b2443e4fdeb7c06587d
unknown <frizi20@yahoo.com>
2022-10-31 18:00:58 +0200

    schema builder

--- resources/views/multiForm.blade.php
+++ resources/views/multiForm.blade.php
@@ -62,9 +62,10 @@
 			animation-direction: reverse; */
 
 			height: 10px;
- 			 /* margin: 50px 0px; */
- 			 background: rgb(255, 255, 255);
-  			position:relative; /* relative here */
+			/* margin: 50px 0px; */
+			background: rgb(255, 255, 255);
+			position: relative;
+			/* relative here */
 
 			/* background: #e5405e; */
 			/* Old browsers */
@@ -75,33 +76,33 @@
 			/* background: linear-gradient(to right, #e5405e 0%, #ffdb3a 25%, #3fffa2 50%, #3fffa2 50%, #1a9be0 73%, #ba68ed 100%); */
 		}
 
-		.progress-bar-indicator{
+		.progress-bar-indicator {
 			/* position: absolute;	 */
 			height: 100%;
 			/* width: 100%; */
 			/* overflow: hidden; */
 			width: 10px;
-			-webkit-mask:linear-gradient(#fff 0 0);
-          	mask:linear-gradient(#fff 0 0);
+			-webkit-mask: linear-gradient(#fff 0 0);
+			mask: linear-gradient(#fff 0 0);
 			border-radius: 5px;
-			transition:width 0.2s linear ;
+			transition: width 0.2s linear;
 		}
 
 		.progress-bar-indicator::before {
-			content:"";
-  			position:absolute;
-  			top:0;
-  			left:0;
-  			right:0;
-  			bottom:0;
-  			background-image: linear-gradient(to right, #166260, #07adce, #3b91ff);
+			content: "";
+			position: absolute;
+			top: 0;
+			left: 0;
+			right: 0;
+			bottom: 0;
+			background-image: linear-gradient(to right, #166260, #07adce, #3b91ff);
 			border-radius: 5px;
 		}
-		
 
-		.multi-form-container .slider{
+
+		.multi-form-container .slider {
 			position: relative;
-			min-height: 150px;
+			min-height: 180px;
 			overflow: hidden;
 
 		}
@@ -114,7 +115,7 @@
 			position: absolute;
 			top: 0;
 			left: 0;
-			transition: all 0~.5s ease-out;
+			transition: all 0.3s ease-out;
 		}
 
 		.fs-title {
@@ -130,6 +131,8 @@
 			margin-top: 20px;
 			display: flex;
 			align-items: center;
+			display: flex;
+			flex-direction: column;
 		}
 
 		.fs-input select {
@@ -151,6 +154,15 @@
 			appearance: none;
 			/* border-radius: 0.375rem; */
 			transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+		}
+
+		.fs-input .error {
+			padding: 6px 0;
+			width: 100%;
+			font-size: 14px;
+			color: #ff4949;
+			text-align: center;
+			display: none;
 		}
 
 		.fs-subtitle {
@@ -176,6 +188,10 @@
 			user-select: none;
 			color: #0070bb;
 		}
+
+		.form-buttons .hide{
+			display: none;
+		}
 	</style>
 
 
@@ -186,13 +202,13 @@
 				<div class="progress-bar-indicator"></div>
 			</div>
 			<div class="slider">
-				<fieldset>
 
+				{{-- <fieldset>
 					<div class="fs-title">Intrebarea 4</div>
 					<div class="fs-subtitle">
 						What postgraduate qualifications do you hold?
 					</div>
-	
+
 					<div class="input-slider">
 						<div class="fs-input">
 							<select name="" id="">
@@ -201,31 +217,15 @@
 								<option value="3">Cluj</option>
 							</select>
 						</div>
-	
-					</div>
-				</fieldset>
-				<fieldset>
 
-					<div class="fs-title">Intrebarea 4</div>
-					<div class="fs-subtitle">
-						What postgraduate qualifications do you hold?
 					</div>
-	
-					<div class="input-slider">
-						<div class="fs-input">
-							<select name="" id="">
-								<option value="1">Bucuresti</option>
-								<option value="2">Brasov</option>
-								<option value="3">Cluj</option>
-							</select>
-						</div>
-	
-					</div>
-				</fieldset>
+				</fieldset> --}}
+
 			</div>
 			<div class="form-buttons">
 				<div class="prev-btn">Previous</div>
 				<div class="next-btn">Next</div>
+				<div class="send-btn hide">Finish</div>
 				{{-- <div class="-btn"></div> --}}
 			</div>
 		</div>
@@ -235,9 +235,8 @@
 
 
 	<script>
-
 		// (function(){
-			document.querySelector('.slider').innerHTML =  document.querySelectorAll('.slider fieldset')[0].outerHTML.repeat(10)
+			// document.querySelector('.slider').innerHTML =  document.querySelectorAll('.slider fieldset')[0].outerHTML.repeat(10)
 		// })();
 
 		const prevBtn = document.querySelector('.prev-btn');
@@ -247,67 +246,353 @@
 		const progressBar = document.querySelector('.progress-bar-indicator')
 
 		let currQuestion = 0
-		let questionNr = questions.length;
+		let questionNr
 
 
-		console.log(questions)
-
-		nextBtn.addEventListener('click',(e)=>{
-			console.log(e)
-		})
-
-		document.querySelector('.form-buttons').addEventListener('click',(e)=>{
-			if(e.target.className.includes('next-btn')) nextQuestion()
-			if(e.target.className.includes('prev-btn')) prevQuestion()
-		})
-
-		const goToQuestion = function(questionNr){
-			Array.from(questions).forEach((question, i) => {
-				// console.log(`translateX(${100 * (i - questionNr)})`)
-				
-				// console.log(progressBar.style.width = '30%')
-				
-				const margin = 0
-				const scale = questionNr == i ? 'scale(1)' : 'scale(0.5)'
-				
-				question.style.transform = `translateX(${100 * (i - questionNr) + margin }%) ${scale}`
-
-				// progressBar.style.width = `${currQuestion / questionNr * 100}%`
-			});
+		const schema = {
+			fields:[
+				{
+					title:'Ai mai furat la locul de munca?',
+					fieldOrder:1,
+					options: [
+						{
+							value:2,
+							label:'Da'
+						},
+						{
+							value:3,
+							label:'Nu'
+						},
+						{
+							value:5,
+							label:'Poate'
+						}
+					]
+				},
+				{
+					title:'Ce faci?',
+					fieldOrder:2,
+					options: [
+						{
+							value:2,
+							label:'Bine'
+						},
+						{
+							value:3,
+							label:'Rau'
+						},
+						{
+							value:5,
+							label:'Habar n-am'
+						}
+					]
+				},
+				{
+					title:'Iti faci treaba?',
+					fieldOrder:3,
+					options: [
+						{
+							value:2,
+							label:'Bine'
+						},
+						{
+							value:3,
+							label:'Rau'
+						},
+						{
+							value:5,
+							label:'Habar n-am'
+						}
+					]
+				},
+				{
+					title:'Iti faci treaba?',
+					fieldOrder:4,
+					options: [
+						{
+							value:2,
+							label:'Bine'
+						},
+						{
+							value:3,
+							label:'Rau'
+						},
+						{
+							value:5,
+							label:'Habar n-am'
+						}
+					]
+				},
+				{
+					title:'Iti faci treaba?',
+					fieldOrder:5,
+					options: [
+						{
+							value:2,
+							label:'Bine'
+						},
+						{
+							value:3,
+							label:'Rau'
+						},
+						{
+							value:5,
+							label:'Habar n-am'
+						}
+					]
+				},
+			]
 		}
 
-		function nextQuestion() {
-			if(currQuestion >= questionNr -1) return
-			currQuestion++
-			goToQuestion(currQuestion)
-			updateProgressBar()
+		class SurveyBuilder {
+
+			fields
+			fieldsLocationDOM
+			nrQuestions
+			currQuestion = 0
+			currSelect
+			outputSchema = []
+			createdFieldsDOM = []
+
+			constructor(schema, fieldsLocationDOM){
+				//create copy of the fields
+				this.fields = schema.fields
+				this.fieldsLocationDOM = fieldsLocationDOM
+				this.createAllFields()
+				this.goToQuestion(0)
+				this.nrQuestions = document.querySelectorAll('.slider fieldset').length
+				this.updateProgressBar()
+
+				this.sendBtnDOM = document.querySelector('.form-buttons .send-btn')
+				document.querySelector('.form-buttons').addEventListener('click',(e)=>{
+					if(e.target.className.includes('next-btn')) this.nextQuestion(e)
+					if(e.target.className.includes('prev-btn')) this.prevQuestion(e)
+					if(e.target.className.includes('send-btn')) this.sendSurvey(e)
+				})
+
+				console.log(this.fields)
+			}
+
+			createAllFields(){
+				// console.log(schema.fields)
+				this.fields.forEach((field,index)=>{
+
+					//Add unique id to be synced with the schema
+					const id = this._createInputId(10)
+					//Add field as select dom element
+					const createdField = this.createField(field,index, id)
+					this.createdFieldsDOM.push(createdField)
+
+					//Add props in outputSchema
+					this.outputSchema.push({
+						id:id,
+						title:field.title,
+						options:field.options
+					})
+
+					//Add events to each fields that updates the schema
+					createdField.addEventListener('change',this.updateSchema.bind(this))
+				})
+
+
+			}
+
+			updateSchema(e){
+				const questionId = e.target.dataset.id
+				const outputField = this.outputSchema.find(field=> field.id == questionId)
+				const selectedValue = e.target.value
+
+				if(!outputField) throw new Error('field not found')
+
+				const valueAllowed = outputField.options.find(el=> el.value == selectedValue)
+				if(!valueAllowed) throw new Error('value not allowed')
+
+				outputField.value = selectedValue
+
+
+			}
+
+
+			createField(field,questionNr, id){
+				const {title,options} = field
+
+				let html = `<fieldset data-id='${id}'>
+								<div class="fs-title">Intrebarea ${questionNr + 1}</div>
+								<div class="fs-subtitle">
+									${title}
+								</div>
+
+								<div class="input-slider">
+									<div class="fs-input">
+										<select name="" id="" data-id='${id}'>
+											<option disabled hidden selected>
+
+											</option>
+											${options.map((opt,index)=>{
+												return `<option value="${opt.value}">${opt.label}</option>`
+											})}
+										</select>
+										<div class="error">
+											Raspuns obligatoriu
+										</div>
+									</div>
+
+								</div>
+						</fieldset>`
+
+				this.fieldsLocationDOM.insertAdjacentHTML('beforeend',html)
+
+				// return created dom element
+				return document.querySelector(`fieldset[data-id="${id}"]`)
+			}
+
+
+
+			goToQuestion(questionNr){
+				Array.from(document.querySelectorAll('.slider fieldset')).forEach((question, i) => {
+
+					const margin = 0
+					const scale = questionNr == i ? 'scale(1)' : 'scale(0.5)'
+					const opacity = questionNr == i ? '1'      : '0.2'
+
+					question.style.transform = `translateX(${100 * (i - questionNr) + margin }%) ${scale}`
+					question.style.opacity = opacity
+				});
+			}
+
+			sendSurvey(e){
+				console.log(
+					this.outputSchema
+				)
+			}
+
+
+			nextQuestion(e) {
+				const nextBtn = e.target
+
+				//Wait until option is selected
+				const currSelect = this.createdFieldsDOM[this.currQuestion].querySelector('select')
+
+				if(!currSelect.value) return
+
+				if(this.currQuestion >= this.nrQuestions -1) return
+
+				//Show SEND button and hide NEXT for the last question
+				if(this.currQuestion >= this.nrQuestions -2){
+					nextBtn.classList.add('hide')
+					this.sendBtnDOM.classList.remove('hide')
+				}
+
+
+
+				this.currQuestion++
+
+				console.log()
+
+				//move UI to next question
+				this.goToQuestion(this.currQuestion)
+				this.updateProgressBar()
+
+			}
+
+			prevQuestion(){
+				if(this.currQuestion === 0) return
+				this.currQuestion--
+				//move UI to prev question
+				this.goToQuestion(this.currQuestion)
+				this.updateProgressBar()
+
+				nextBtn.classList.remove('hide')
+				this.sendBtnDOM.classList.add('hide')
+
+			}
+
+			 updateProgressBar(){
+				progressBar.style.width =( this.currQuestion + 1) / this.nrQuestions * 100 + '%'
+			}
+
+			_createInputId(randStringLength){
+
+				if(!randStringLength || randStringLength < 1) return
+
+				const alphabet = [...Array(26).keys()].map(i => String.fromCharCode(i + 97));
+				const nrs = [1,2,3,4,5,6,7,8,9]
+				const hex = [...alphabet, ...nrs]
+				const random = function(){
+					return Math.floor(Math.random() * hex.length)
+				}
+				return [...Array(randStringLength)].map(()=>hex[random()]).join('')
+
+			}
 		}
 
-		function prevQuestion(){
-			if(currQuestion === 0) return
-			currQuestion--
-			goToQuestion(currQuestion)
-			updateProgressBar()
-		}
-
-		function updateProgressBar(){
-			progressBar.style.width =( currQuestion + 1) / questionNr * 100 + '%'
-		}
-
-		goToQuestion(currQuestion)
-		updateProgressBar(1)
+		const surveyBuilder = new SurveyBuilder(schema, slider)
 
 
 
+		//slider navigation
+		// document.querySelector('.form-buttons').addEventListener('click',(e)=>{
+		// 	if(e.target.className.includes('next-btn')) nextQuestion()
+		// 	if(e.target.className.includes('prev-btn')) prevQuestion()
+		// })
 
-		function createField(){
+		// const goToQuestion = function(questionNr){
+		// 	Array.from(document.querySelectorAll('.slider fieldset')).forEach((question, i) => {
+		// 		// console.log(`translateX(${100 * (i - questionNr)})`)
+
+		// 		// console.log(progressBar.style.width = '30%')
+
+		// 		const margin = 0
+		// 		const scale = questionNr == i ? 'scale(1)' : 'scale(0.5)'
+
+		// 		question.style.transform = `translateX(${100 * (i - questionNr) + margin }%) ${scale}`
+
+		// 		// progressBar.style.width = `${currQuestion / questionNr * 100}%`
+		// 	});
+		// }
+
+		// function nextQuestion() {
+		// 	console.log({
+		// 		currQuestion,
+		// 		questionNr
+		// 	})
+		// 	if(currQuestion >= questionNr -1) return
+		// 	currQuestion++
+		// 	goToQuestion(currQuestion)
+		// 	updateProgressBar()
+		// }
+
+		// function prevQuestion(){
+		// 	if(currQuestion === 0) return
+		// 	currQuestion--
+		// 	goToQuestion(currQuestion)
+		// 	updateProgressBar()
+		// }
+
+
+
+		// goToQuestion(currQuestion)
+		// updateProgressBar(1)
+
+
+		// function createAllFields(){
+		// 	console.log(schema.fields)
+		// 	schema.fields.forEach((el,index)=>{
+		// 		// console.log(el)
+		// 	})
+		// }
+
+
+
+		// function createField(){
+
 				// <fieldset>
 
 				// 	<div class="fs-title">Intrebarea 4</div>
 				// 	<div class="fs-subtitle">
 				// 		What postgraduate qualifications do you hold?
 				// 	</div>
-	
+
 				// 	<div class="input-slider">
 				// 		<div class="fs-input">
 				// 			<select name="" id="">
@@ -316,11 +601,11 @@
 				// 				<option value="3">Cluj</option>
 				// 			</select>
 				// 		</div>
-	
+
 				// 	</div>
 				// </fieldset>
-		}
-		
+		// }
+
 
 	</script>
 

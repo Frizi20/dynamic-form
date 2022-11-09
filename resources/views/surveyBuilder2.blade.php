7eb1be9bd3536870869a00728426efad68f46c90
unknown <frizi20@yahoo.com>
2022-11-09 17:57:17 +0200

    move list

--- resources/views/surveyBuilder2.blade.php
+++ resources/views/surveyBuilder2.blade.php
@@ -22,13 +22,15 @@
 			padding: 0;
 			box-sizing: border-box;
 			min-height: 1000px;
-			height: 100%;
+			/* height: 100%; */
 			background-color: #fff;
 			font-family: sans-serif;
 			font-family: 'Open Sans', sans-serif;
-			,
-			sans-serif;
 
+		}
+
+		body {
+			padding: 100px 0;
 		}
 
 		*,
@@ -48,8 +50,9 @@
 		.form-builder-container {
 			min-height: 500px;
 			width: 700px;
-			border: 1px solid grey;
+			border: 1px solid #e4e4e4;
 			overflow: auto;
+			position: relative;
 		}
 
 		.form-json {
@@ -63,6 +66,7 @@
 			width: 500px;
 			height: 500px;
 			margin-right: 15px;
+			display: none;
 			/* overflow-y: auto; */
 		}
 
@@ -95,6 +99,8 @@
 			font-weight: 700;
 			overflow: hidden;
 			position: relative;
+			position: sticky;
+			top: 0;
 		}
 
 		/*
@@ -121,6 +127,7 @@
 
 		.form-builder-container {
 			padding: 20px;
+			/* max-height: 600px; */
 		}
 
 		.form-field {
@@ -154,6 +161,27 @@
 
 		}
 
+		.form-group select {
+			width: 100%;
+			padding: 4px;
+			font-size: 15px;
+			display: block;
+			width: 100%;
+			padding: 0.175rem 0.75rem;
+			font-size: 1rem;
+			font-weight: 400;
+			line-height: 1.5;
+			color: #7b8085;
+			background-color: #fff;
+			background-clip: padding-box;
+			border: 1px solid #ced4da;
+			-webkit-appearance: none;
+			-moz-appearance: none;
+			appearance: none;
+			/* border-radius: 0.375rem; */
+			transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+		}
+
 		.form-group.hidden {
 			display: none;
 		}
@@ -181,12 +209,15 @@
                                   supported by Chrome, Edge, Opera and Firefox */
 			word-spacing: 1px;
 			letter-spacing: 0.5px;
+			margin-left: 5px;
+			display: block;
+			max-height: 300px;
+			/* transition: max-height opacity 5s; */
+
 
 		}
 
-		.field-label.hidden {
-			display: none;
-		}
+
 
 		.form-control {
 			display: block;
@@ -254,22 +285,39 @@
 			width: 100%;
 			/* height: 200px; */
 			background-color: #fff;
-			display: none;
+			display: block;
 			position: relative;
-			padding: 20px;
-			padding-bottom: 60px;
+			padding: 0;
+			opacity: 0;
+
+			/* padding-bottom: 60px; */
+			max-height: 0;
+			transition: max-height 0.3s, padding-bottom 0.3s, padding-top 0.3s, opacity 0.3s linear;
 		}
 
 		.edit-field.active {
-			display: block;
+			/* display: block; */
+			max-height: 500px;
+			padding: 20px 20px 60px 20px;
+			/* padding-bottom: 60px; */
+			opacity: 1;
 		}
 
 		.form-group.hidden {
-			display: none;
+			max-height: 0;
+			/* display: none; */
+			transition: all 0.3s linear;
 		}
 
 		.field-label.hidden {
 			display: none;
+			/* font-size: 0; */
+			/* margin: 0 !important; */
+			/* opacity: 0; */
+			/* padding: 0; */
+			/* max-height: 0; */
+			/* fade out, then shrink */
+			/* transition: all 0.3s linear; */
 		}
 
 		.edit-field-options,
@@ -394,6 +442,12 @@
 			background: whitesmoke;
 			bottom: -2px;
 			color: #505050;
+			opacity: 0;
+		}
+
+		.edit-field.active .close-options {
+			opacity: 1;
+			transition: all 0.2s linear;
 		}
 	</style>
 
@@ -429,6 +483,12 @@
 			allFields
 			allFieldsDOM = []
 
+			draggedField
+			fieldIsDragged = false
+			pointerX
+			pointerY
+			fieldX
+			fieldY
 
 			constructor(schema){
 				this.formBuilderDOM = document.querySelector('.form-builder')
@@ -441,6 +501,8 @@
 				this._createFields()
 
 				this._sortFields(this.allFields)
+
+
 			}
 
 			_createFields(){
@@ -459,15 +521,130 @@
 					//add delete option for the field
 					this._addDeleteEvent(createdField)
 
-					//add edit event
-
-					this._addEditEvent(createdField)
-
 					//add delete option event
-
 					this._addDeleteOptionEvent(createdField)
 
+					//add edit event
+					this._addEditEvent(createdField)
+
+					//add the add+ option functionality
+					this._addAugmentOpion(createdField)
+
+
+					//add update description
+					this._addUpdateTitleEvent(createdField)
+
+					//add update option value event
+					this._addUpdateOptionValue(createdField)
+
+
+					//add update label event
+					this._addUpdateOptionLabel(createdField)
+
+					//add draggable functionality to every field
+					this._addDraggingEvents(createdField)
+
 				})
+			}
+
+			_addDraggingEvents(fieldDOM){
+				fieldDOM.addEventListener('mousedown', this._clickDown.bind(this), true)
+				document.addEventListener('mousemove', this._moveEl.bind(this),true)
+				document.addEventListener('mouseup', this._clickUp.bind(this), true)
+			}
+
+			_clickDown(e){
+
+				const element = e.target
+				if(!element.className.includes('form-field')) return
+
+				const {width,height,left,top} = element.getBoundingClientRect()
+
+				this.fieldClone = element.cloneNode(true)
+				this.fieldClone.style.filter = 'blur(50px)'
+
+				element.insertAdjacentElement('beforebegin',this.fieldClone)
+
+				Object.assign(element.style, {
+					position:'fixed',
+					width:`${ width}px`,
+					height: `${height}px`,
+					left: `${window.scrollX + left}px`,
+					top: `${top - 20}px`,
+					zIndex:200,
+					opacity:0.8
+				})
+
+				this.pointerX = e.clientX
+				this.pointerY = e.clientY
+				this.elInitialX = left
+				this.elInitialY = top  - 20
+
+				this.fieldIsDragged = true
+				this.draggedField = element
+
+
+				console.log(top)
+
+			}
+
+			_moveEl(e){
+				if(!this.fieldIsDragged) return
+
+				const moveX = (this.pointerX - e.clientX) * -1
+				const moveY = (this.pointerY - e.clientY) * -1
+
+
+				const currX = this.pointerX
+				const currY = this.pointerY
+				const elementCurrentX = this.elInitialX + moveX
+				const elementCurrentY = this.elInitialY + moveY
+				const {left:currElX,top:currElY,height:currElHeight,width:currElWidth} = this.draggedField.getBoundingClientRect()
+
+				this.draggedField.style.left = (elementCurrentX) + 'px'
+				this.draggedField.style.top = (elementCurrentY) + 'px'
+				// this.draggedField.style.position = 'fixed'
+				// this.draggedField.style.transform = currY - e.clientY
+
+				// this.draggedField.style.transform = 'translateX(-20px)'
+				this.allFieldsDOM.map((draggable,i)=>{
+					const {left:draggableX,top:draggableY,height:draggableHeight,width:draggableWidth} = draggable.getBoundingClientRect()
+
+					if(draggable !== this.draggedField){
+
+						if(((currElHeight + currElY - 30)> draggableX) &&
+							currElY + 25 < draggableX + draggableHeight){
+
+								draggable.style.opacity = 0.3
+								// allDraggables.lastDraggedOverEl = draggable.draggedElement
+								// this.lastDragged = draggable.draggedElement
+								// draggable.draggedElement.style.opacity = 0.5
+								console.log('da')
+
+
+						}else{
+							draggable.style.opacity = 1
+						}
+
+					}
+
+				})
+			}
+
+			_clickUp(e){
+				// if(!e.target.className.includes('form-field')) return
+				this.fieldIsDragged = false
+				if(!this.draggedField) return
+				Object.assign(this.draggedField.style,{
+					position:'relative',
+					width:'auto',
+					height:'auto',
+					left:0,
+					top:0,
+					zIndex:0,
+					opacity:1
+				})
+				this.fieldClone.remove()
 			}
 
 			_sortFields(fields){
@@ -485,9 +662,22 @@
 				const newFieldOrder = this.allFields[this.allFields.length - 1]?.fieldOrder + 1
 
 				const fieldData = {
-					title:'nedefinit',
-					fieldOrder:newFieldOrder,
-					options:[],
+					title:'(Descriere)',
+					fieldOrder:newFieldOrder || 1,
+					options:[
+						{
+							value:'valoare 1',
+							label:'raspuns 1'
+						},
+						{
+							value:'valoare 2',
+							label:'raspuns 2'
+						},
+						{
+							value:'valoare 2',
+							label:'raspuns 3'
+						}
+					],
 					id:newId
 				}
 
@@ -501,12 +691,32 @@
 				//add deete option for the new field
 				this._addDeleteEvent(createdNewField)
 
-				//add edit option
+				//add delete option event
+				this._addDeleteOptionEvent(createdNewField)
 
+				//add edit option
 				this._addEditEvent(createdNewField)
 
 				//add data to the state
 				this.allFields.push(fieldData)
+
+				//add the add+ option functionality
+				this._addAugmentOpion(createdNewField)
+
+				//add update description
+				this._addUpdateTitleEvent(createdNewField)
+
+				//add update option value event
+				this._addUpdateOptionValue(createdNewField)
+
+
+				//add update label event
+				this._addUpdateOptionLabel(createdNewField)
+
+			}
+
+			_addAugmentOpion(createdField){
+				createdField.querySelector('.option-actions span').addEventListener('click',this._addOption.bind(this))
 
 			}
 
@@ -514,14 +724,163 @@
 				createdField.querySelector('.remove-input').addEventListener('click', this._deleteField.bind(this))
 			}
 
+			_addUpdateTitleEvent(createdField){
+				createdField.querySelector('.edit-field-title .title').addEventListener('change',this._updateTitle.bind(this))
+			}
+			_addUpdateOptionValue(createdField){
+				createdField.querySelectorAll('.option .value input').forEach(optLabel =>{
+					optLabel.addEventListener('change',this._updateValue.bind(this))
+				})
+			}
+
+			_addUpdateOptionLabel(createdField){
+				createdField.querySelectorAll('.option .label input').forEach(optLabel =>{
+					optLabel.addEventListener('change',this._updateLabel.bind(this))
+				})
+			}
+
 			_addDeleteOptionEvent(createdField){
 				createdField.querySelectorAll('.remove-option').forEach(delOption => {
-					delOption.addEventListener('click',function(){
-						console.log(this)
-					})
+					delOption.addEventListener('click',this._deleteOption.bind(this))
 				});
 			}
 
+			_updateTitle(e){
+				const descInput = e.target
+				const currField = descInput.closest('.form-field')
+				const fieldId = currField.dataset.inputId
+				const fieldLabelDOM = currField.querySelector('.field-label')
+				const currFieldState = this.allFields.find(field => fieldId == field.id)
+
+				// change field state
+				currFieldState.title = descInput.value
+
+				//change field description Value
+				fieldLabelDOM.textContent = descInput.value
+
+			}
+
+			_updateLabel(e){
+				const labelInput = e.target
+				const optionId = labelInput.closest('.option').dataset.optionId
+				const currField = labelInput.closest('.form-field')
+				const formFieldId = currField.dataset.inputId
+				const currFieldState = this.allFields.find(field => field.id == formFieldId)
+				const currOptionState = currFieldState.options.find(option=> option.id == optionId)
+
+				const optionDOM = currField.querySelector(`.form-group [data-option-id="${optionId}"]`)
+
+				console.log(optionDOM)
+				//change the option label from state
+				currOptionState.label = labelInput.value
+
+				//change option label DOM
+				optionDOM.textContent = labelInput.value
+
+			}
+
+			_updateValue(e){
+				const valueInput = e.target
+
+				const optionId = valueInput.closest('.option').dataset.optionId
+				const currField = valueInput.closest('.form-field')
+				const formFieldId = currField.dataset.inputId
+				const currFieldState = this.allFields.find(field => field.id == formFieldId)
+				const currOptionState = currFieldState.options.find(option=> option.id == optionId)
+
+				const optionDOM = currField.querySelector(`.form-group [data-option-id="${optionId}"]`)
+
+				console.log(valueInput)
+				//change the option label from state
+				currOptionState.value = valueInput.value
+
+				//change option label DOM
+				console.log(optionDOM)
+				optionDOM.value = valueInput.value
+
+			}
+
+			_addOption(e){
+				const addOptionBtn =  e.target.closest('.option-actions span')
+				if(!addOptionBtn) return
+				const fieldID = addOptionBtn.dataset.inputId
+				const newOptionId = this._createInputId(10)
+				const newOptionData = {
+					value:'',
+					label:'',
+					id:newOptionId
+				}
+
+
+				const optionHTML = `<div class ="option" data-option-id="${newOptionId}">
+										<div class="label">
+											<input type="text" value="${''}" placeholder="label" >
+										</div>
+										<div class="value">
+											<input type="text" value="${''}" placeholder="value">
+										</div>
+										<div class="remove-option" data-option-id="${newOptionId}">
+											<i class="fa fa-times" aria-hidden="true"></i>
+										</div>
+									</div>`
+
+				//Add new option to the field schema
+				this.allFields.find(field => fieldID == field.id).options.push(newOptionData)
+
+				//Add option to the DOM
+				// --- Add DOM option to the edit Modal
+				addOptionBtn.parentElement.insertAdjacentHTML('beforebegin', optionHTML)
+				// --- Add DOM option to the select input preview
+                const currSelect = document.querySelector(`.form-field[data-input-id="${fieldID}"] .form-group select`)
+				this._createSelectOption(currSelect,newOptionId)
+
+
+
+				//add delete event to remove-option btn
+				const removeBtn = document.querySelector(`.remove-option[data-option-id = "${newOptionId}"]`)
+				removeBtn.addEventListener('click', this._deleteOption.bind(this))
+
+				//add update value and label events to the new option DOM
+				const optionDom = document.querySelector(`.option[data-option-id="${newOptionId}"]`)
+				const updateLabelInput = optionDom.querySelector('.label input')
+				const updateValueInput = optionDom.querySelector('.value input')
+
+				updateLabelInput.addEventListener('change',this._updateLabel.bind(this))
+				updateValueInput.addEventListener('change',this._updateValue.bind(this))
+			}
+
+			_createSelectOption(selectEl,newOptionId){
+				const optionHTML = `<option value="5" data-option-id="${newOptionId}"> </option>`
+				selectEl.insertAdjacentHTML('beforeend',optionHTML)
+			}
+
+			_deleteOption(e){
+				const optionClicked = e.target.closest('.remove-option')
+				if(!optionClicked) return
+				const optionId = optionClicked.dataset.optionId
+				const formFieldId = optionClicked.closest('.form-field').dataset.inputId
+
+				//remove option from the edit DOM
+				const currFieldState = this.allFields.filter(field =>{
+					return field.id == formFieldId
+				})
+
+				//if there are less than one option stop removing the option
+				if(currFieldState[0].options.length <= 2) return
+
+				//remove option from the edit Modal DOM
+				optionClicked.parentElement.remove()
+
+				//remove option from the select preview
+				document.querySelector(`option[data-option-id="${optionId}"]`).remove()
+
+				//remove option from the field options in the schema
+				const option = currFieldState[0].options.splice(currFieldState[0].options.findIndex(option => option.id == optionId),1)
+
+
+
+				console.log(option)
+			}
 
 			_deleteField(e){
 				const btnClicked = e.target.closest('.remove-input')
@@ -537,8 +896,13 @@
 					formField.remove()
 				}, 250);
 
+
 				//remove fromAllFields array
 				this.allFields.splice((this.allFields.findIndex(field=> field.id === id)),1)
+
+				//remove from allFieldsDOM
+				this.allFieldsDOM.splice(this.allFieldsDOM.findIndex(fieldDOM=> fieldDOM == formField),1)
+
 
 
 			}
@@ -568,13 +932,14 @@
 				editField.classList.toggle('active')
 
 
-				console.log(formField)
 			}
 
 			_createField(field,id){
 
+				let optionsIds = []
+
 				const html = `
-				<div class="form-field" data-input-id="${id}" draggable="true" >
+				<div class="form-field" data-input-id="${id}"  >
 					<label class="field-label"> ${field.title} </label>
 					<div class="field-actions">
 						<div class="edit-input" data-input-id="${id}">
@@ -585,7 +950,15 @@
 						</div>
 					</div>
 					<div class="form-group">
-						<input type="string" class="form-control">
+						<select>
+							${field.options.map((option,index) =>{
+								const optionId = this._createInputId(10)
+								optionsIds.push(optionId)
+								return `<option value="${option.value}" data-option-id="${optionId}">
+											${option.label}
+										</option>`
+							}).join('')}
+						</select>
 					</div>
 					<div class="edit-field">
 						<div class="edit-field-title">
@@ -602,28 +975,30 @@
 								Options
 							</div>
 							<div class="options">
+
 								${field.options.map((option,index,optionsArr ) =>{
 
-									const optionId = this._createInputId(10)
+									const optionId = optionsIds[index]
 
 									//Set option id to the schema
-									optionsArr[index][id] = optionId
+									optionsArr[index]['id'] = optionId
 
 									return`<div class ="option" data-option-id="${optionId}">
 
 										<div class="label">
-											<input type="text" value="${option.label}">
+											<input type="text" value="${option.label}" placeholder="label">
 										</div>
 										<div class="value">
-											<input type="text" value="${option.value}">
+											<input type="text" value="${option.value}" placeholder="value" >
 										</div>
 										<div class="remove-option" data-option-id="${optionId}">
 											<i class="fa fa-times" aria-hidden="true"></i>
 										</div>
 									</div>`
 								}).join('')}
+
 								<div class="option-actions">
-									<span> Add Option + </span>
+									<span data-input-id="${id}"> Add Option + </span>
 								</div>
 							</div>
 						</div>
@@ -640,7 +1015,7 @@
 
 			}
 
-			_createInputId(randStringLength){
+			_createInputId(randStringLength = 10){
 
 				if(!randStringLength || randStringLength < 1) return
 
